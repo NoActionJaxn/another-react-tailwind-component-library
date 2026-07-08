58df4feb74914d051b9281fbd328075fb25b9204
NoActionJaxn <jahermitt@gmail.com>
2026-07-08 00:53:05 -0400

    Added tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import "./styles/main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <>This is an empty app</>
   </StrictMode>,
 );
